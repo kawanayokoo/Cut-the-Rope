7140aae1c676b531d2cc4305e0d60c6bdec69079
kawanayokoo <94402205+kawanayokoo@users.noreply.github.com>
2022-07-25 16:06:42 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var rope1;
+var fruit;
+var FlinkR;
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+  rope1 = new Rope(7,{x:245,y:30});
+  FlinkR = new Link(rope1,fruit);
+
+  var fruit_options = {
+    density: 0.006
+  }
+
+  fruit = Bodies.circle(275,275,25,fruit_options);
+  Matter.Composite.add(rope1.body, fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  rope1.show();
+  ellipse(fruit.position.x,fruit.position.y,25,25);
+  
+  Engine.update(engine);
+  
+
+ 
+   
+}
